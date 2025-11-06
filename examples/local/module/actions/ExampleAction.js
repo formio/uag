be21e8fe20528ea292f58871b4fca432fc31662c
Travis Tidwell <travis@form.io>
2025-11-06 08:42:00 -0600

    Adding better example action documentation.

--- examples/local/module/actions/ExampleAction.js
+++ examples/local/module/actions/ExampleAction.js
@@ -17,7 +17,10 @@ export const ExampleAction = {
 
     /**
      * The settings form for this action.
-     * @param {import('../../../../src').FormInterface} form - The form that this action is being configured for.
+     * 
+     * @import { FormInterface } from '@formio/appserver';
+     * @param { FormInterface } form - The form that this action is being configured for.
+     * @returns - Form.io Form JSON schema for the project settings for this action.
      */
     async settingsForm(form) {
         return [
@@ -30,11 +33,50 @@ export const ExampleAction = {
         ];
     },
 
+    /**
+     * Enterprise Only: This provides a way to store "global" project settings that are encrypted. This is useful
+     * if your action needs to use sensitive information to properly execute. For example, if you are creating an
+     * action that integrates with a third-party service, you may need to store an API key or secret.
+     *
+     * @import { FormInterface } from '@formio/appserver';
+     * @param {FormInterface} form 
+     * @returns - Form.io Form JSON schema for the project settings for this action.
+     */
+    async projectSettings(form) {
+        return [
+            {
+                type: 'textfield',
+                key: 'superSecret',
+                label: 'Super Secret Setting',
+                input: true
+            }
+        ];
+    },
+
+    /**
+     * @import { FormAction } from '@formio/core';
+     * @import { FormInterface } from '@formio/appserver';
+     * @param { FormInterface } form 
+     * @param { FormAction } action 
+     * @param { 'before' | 'after' } handler 
+     * @param { 'create' | 'update' | 'delete' } method 
+     * @returns 
+     */
     async executor(form, action, handler, method) {
         const settings = action.settings;
         console.log(settings.example); // This is the value configured in the settings form for this action instance.
+
+        /**
+         * The executor function that is returned here is what is executed when the action is triggered.
+         * 
+         * @import { Request, Response, NextFunction } from 'express';
+         * @param { Request } req - The Express.js request object.
+         * @param { Response } res - The Express.js response object.
+         * @param { NextFunction } next - The Express.js next function to call the next middleware.
+         */
         return async (req, res, next) => {
             // The action is executed here as Express.js middleware.
+            console.log(form.project?.settings); // The encrypted project settings for Enterprise Only.
             console.log(req.body);  // The submission data if the handler is "before"
             console.log(res.resource); // The "response" data if the handler is "after";
             next(); // Call next to continue processing.
